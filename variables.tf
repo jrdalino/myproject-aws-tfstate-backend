bdbb2003ff076b975487b9fb1eaa668b8a758ea0
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-01-29 21:11:55 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+# General
+variable aws_region {
+  type        = string
+  description = "AWS Region."    
+}  
+
+variable aws_profile {
+  type        = string
+  description = "AWS Profile."    
+}
+
+variable "aws_account" {
+  type        = string
+  description = "AWS Account."    
+}
+
+# S3 Bucket
+variable aws_s3_bucket_bucket {
+  type        = string
+  description = "(Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name."
+}
+
+# DynamoDB Table
+
+variable aws_dynamodb_table_name {
+  type        = string
+  description = "The name of the table, this needs to be unique within a region."    
+}
+
+variable aws_dynamodb_table_hash_key {
+  type        = string
+  description = "(Required, Forces new resource) The attribute to use as the hash (partition) key. Must also be defined as an attribute, see below."    
+}
+
+variable aws_dynamodb_table_read_capacity {
+  type        = string
+  description = "(Optional) The number of read units for this table. If the billing_mode is PROVISIONED, this field is required."    
+}
+
+variable aws_dynamodb_table_write_capacity {
+  type        = string
+  description = "(Optional) The number of write units for this table. If the billing_mode is PROVISIONED, this field is required."    
+}
