e230ba845ebacb5f15ad9b75ae7b8688f5a8b9dd
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-01-29 21:10:00 +0800

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+terraform {
+  required_version = "~> 0.12.0"
+
+  required_providers {
+    aws   = "~> 2.0"
+  }
+}
